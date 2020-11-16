9bbb7cb5997ae7f4a0f5715765ac90204d1429cb
Caitlin O'Callaghan <caitlinocallaghan22@gmail.com>
2020-11-15 23:09:57 -0800

    run aws proxy scripts from repo main directory

--- mediastore_launch_proxy.sh
+++ mediastore_launch_proxy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export UPLOAD_DRIVER=mediastore BUCKET_NAME=dropbears REGION_NAME=us-west-2 HTTP_PORT=8080
+cd s3-upload-proxy
 go build -o s3-upload-proxy
-./s3-upload-proxy
+cd ..
+./s3-upload-proxy/s3-upload-proxy
 
 
--- s3_launch_proxy.sh
+++ s3_launch_proxy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export UPLOAD_DRIVER=s3 BUCKET_NAME=dopebears REGION_NAME=us-west-2 HTTP_PORT=8080
+cd s3-upload-proxy
 go build -o s3-upload-proxy
-./s3-upload-proxy
+cd ..
+./s3-upload-proxy/s3-upload-proxy
 
 
